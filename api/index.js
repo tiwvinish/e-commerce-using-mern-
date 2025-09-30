ec575e173bc11c1d00ec1935809c598be39437f6
tiwvinish <tiwvinish@gmail.com>
2025-09-30 22:17:03 +0545

    update

--- api/index.js
+++ api/index.js
@@ -44,6 +44,6 @@ app.listen(port,async()=>{
     console.log(`Server started at http://localhost:${port}`)
     console.log('Pres Ctrl+C to stop')
     await mongoose.connect(mongoUrl)
-    console.log('MongoDB Connected')
+    console.log('MongoDB Connected !!')
     
 })
