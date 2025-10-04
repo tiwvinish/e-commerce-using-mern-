04c067234e7800e12a90dd1b6af80961ba2c8665
tiwvinish <tiwvinish@gmail.com>
2025-10-04 21:21:14 +0545

    update

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ app.use((req,res,next)=>{
 app.use((error, req, res, next)=>{
     res.status(error.status || 400)
     .send({
-        message: error.message ||'Something went wrong',
+        message: error.message ||'Something went wrong !!',
         validation:error.validation
 })
 })
