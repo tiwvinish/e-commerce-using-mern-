0c771a36daa720a7da19219f1d15ea05f22720b6
tiwvinish <tiwvinish@gmail.com>
2025-10-07 22:27:40 +0545

    update

--- api/index.js
+++ api/index.js
@@ -42,7 +42,7 @@ app.use((error, req, res, next)=>{
 
 app.listen(port,async()=>{
     console.log(`Server started at http://localhost:${port}`)
-    console.log('Press ctrl+c to stop hai !! ')
+    console.log('Press ctrl+c to stop ! ')
     await mongoose.connect(mongoUrl)
     console.log('MongoDB Connected Successfully !! ')
     
